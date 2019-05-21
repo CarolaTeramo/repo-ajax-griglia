c85e09a5958b12ec33cc6c4247d6d364e3c25df6
CarolaTeramo <carolateramo@gmail.com>
2019-05-21 22:42:46 +0200

    Con modifica sul click sullo stesso quadratino

--- ajax_griglia_main.js
+++ ajax_griglia_main.js
@@ -5,39 +5,75 @@ $(document).ready(function(){
     $('.griglia').append('<div class="quadratino"></div>');
   }
 
+
   $('.quadratino').click(function(){
     var questo = $(this);
-    $.ajax({
-      'url': 'https://www.boolean.careers/api/random/int',
-      'method': 'GET',
-      'success': function(data){
-        output(data);
-        console.log(data);
-      },
-      'error': function(){
+    // se non ha la classe giallo e se non ha la classe verde
+    //fai qualcosa
+    if ( (!questo.hasClass('giallo')) & (!questo.hasClass('verde')) ){
+      
+      $.ajax({
+        'url': 'https://www.boolean.careers/api/random/int',
+        'method': 'GET',
+        'success': function(data){
+          output(data);
+          console.log(data);
+        },
+        'error': function(){
 
-      }
-    });
-    function output(info){
-      // info= generica variabile a cui sopra do valore data
-      var numero = info.response;
-      console.log(numero);
-      console.log(questo);
-      if (numero <= 5 ){
-        //alert('ok')
-        questo.addClass('giallo');
-        questo.append('<div class="scritta"></div>').text(numero);
-        //questo.children('.scritta').text(numero)
-      } else {
-        questo.addClass('verde');
-        questo.append('<div class="scritta"></div>').text(numero);
+        }
+      });
+      function output(info){
+        // info= generica variabile a cui sopra do valore data
+        var numero = info.response;
+        console.log(numero);
+        console.log(questo);
+        if (numero <= 5 ){
+          //alert('ok')
+          questo.addClass('giallo');
+          questo.append('<div class="scritta"></div>').text(numero);
+          //questo.children('.scritta').text(numero)
+        } else {
+          questo.addClass('verde');
+          questo.append('<div class="scritta"></div>').text(numero);
+        }
+
       }
     }
 
 
   });
 
-
+  // $('.quadratino').click(function(){
+  //   var questo = $(this);
+  //   $.ajax({
+  //     'url': 'https://www.boolean.careers/api/random/int',
+  //     'method': 'GET',
+  //     'success': function(data){
+  //       output(data);
+  //       console.log(data);
+  //     },
+  //     'error': function(){
+  //
+  //     }
+  //   });
+  //   function output(info){
+  //     // info= generica variabile a cui sopra do valore data
+  //     var numero = info.response;
+  //     console.log(numero);
+  //     console.log(questo);
+  //     if (numero <= 5 ){
+  //       //alert('ok')
+  //       questo.addClass('giallo');
+  //       questo.append('<div class="scritta"></div>').text(numero);
+  //       //questo.children('.scritta').text(numero)
+  //     } else {
+  //       questo.addClass('verde');
+  //       questo.append('<div class="scritta"></div>').text(numero);
+  //     }
+  //
+  //   }
+  // });
 
 
 
