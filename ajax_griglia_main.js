d108cb864acf5daa7716f58f2ba67eab477af340
CarolaTeramo <carolateramo@gmail.com>
2019-05-21 22:45:40 +0200

    Modifica sul click sullo stesso quadratino

--- ajax_griglia_main.js
+++ ajax_griglia_main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
     // se non ha la classe giallo e se non ha la classe verde
     //fai qualcosa
     if ( (!questo.hasClass('giallo')) & (!questo.hasClass('verde')) ){
-      
+
       $.ajax({
         'url': 'https://www.boolean.careers/api/random/int',
         'method': 'GET',
@@ -44,36 +44,7 @@ $(document).ready(function(){
 
   });
 
-  // $('.quadratino').click(function(){
-  //   var questo = $(this);
-  //   $.ajax({
-  //     'url': 'https://www.boolean.careers/api/random/int',
-  //     'method': 'GET',
-  //     'success': function(data){
-  //       output(data);
-  //       console.log(data);
-  //     },
-  //     'error': function(){
-  //
-  //     }
-  //   });
-  //   function output(info){
-  //     // info= generica variabile a cui sopra do valore data
-  //     var numero = info.response;
-  //     console.log(numero);
-  //     console.log(questo);
-  //     if (numero <= 5 ){
-  //       //alert('ok')
-  //       questo.addClass('giallo');
-  //       questo.append('<div class="scritta"></div>').text(numero);
-  //       //questo.children('.scritta').text(numero)
-  //     } else {
-  //       questo.addClass('verde');
-  //       questo.append('<div class="scritta"></div>').text(numero);
-  //     }
-  //
-  //   }
-  // });
+  
 
 
 
